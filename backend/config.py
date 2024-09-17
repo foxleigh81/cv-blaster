7e0d780805ff05623e1dda13ef1f0b9f142aaae0
Alex Foxleigh <alex@alexfoxleigh.com>
2024-09-17 17:54:19 +0100

    Updates config.py to load dotenv

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,11 @@
-# config.py
-
 import os
+from dotenv import load_dotenv
+
+# Get the base directory of the project
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Load environment variables from the .env file
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     # Get database URL from environment variable or use default
