0789b790dc5d2d660889db4e70f5a1e70e6c3383
Alex Foxleigh <alex@alexfoxleigh.com>
2024-09-17 17:42:51 +0100

    Removes auth endpoint as I've decided to use OAuth for that

--- backend/endpoints/__init__.py
+++ backend/endpoints/__init__.py
@@ -1,6 +1,8 @@
 def register_blueprints(app):
     from endpoints.users import users_bp
-    from endpoints.auth import auth_bp
+    from endpoints.skills import skills_bp
+    from endpoints.history import history_bp
 
     app.register_blueprint(users_bp, url_prefix='/users')
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(skills_bp, url_prefix='/skills')
+    app.register_blueprint(history_bp, url_prefix='/history')
